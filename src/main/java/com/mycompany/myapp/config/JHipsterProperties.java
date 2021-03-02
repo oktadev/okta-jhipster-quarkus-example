3fb146d350c1716e5fa26084e6c4bf7da607b31b
danielpetisme <daniel.petisme@gmail.com>
2021-03-02 23:10:26 +0100

    Initial version of jhipster generated by JHipster-6.10.5 with blueprints: quarkus-1.0.0

---
+++ src/main/java/com/mycompany/myapp/config/JHipsterProperties.java
@@ -0,0 +1,12 @@
+package com.mycompany.myapp.config;
+
+import io.quarkus.arc.config.ConfigProperties;
+
+@ConfigProperties(prefix = "jhipster")
+public class JHipsterProperties {
+    public Oidc oidc;
+
+    public static class Oidc {
+        public String logoutUrl;
+    }
+}
